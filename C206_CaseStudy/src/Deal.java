0302809b19946b031842a9345961196afeeff1df
21011471 <21011471@192.168.0.131>
2022-07-26 22:06:27 +0800

    Deal class - Eileen

--- C206_CaseStudy/src/Deal.java
+++ C206_CaseStudy/src/Deal.java
@@ -1,4 +1,43 @@
 
 public class Deal {
+	String id;
+	String itemName;
+	String sellerEmail;
+	String buyerEmail;
+	int transPrice;
+	String closeDate;
+
+
+	public Deal (String id, String itemName, String sellerEmail, String buyerEmail, int transPrice, String closeDate) {
+		this.id = id;
+		this.itemName = itemName;
+		this.sellerEmail = sellerEmail;
+		this.buyerEmail = buyerEmail ;
+		this.transPrice = transPrice;
+		this.closeDate = closeDate;
+	}
+	public String getId() {
+		return id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public String getSellerEmail() {
+		return sellerEmail;
+	}
+
+	public String getBuyerEmail() {
+		return buyerEmail;
+	}
+
+	public int getTransactionPrice() {
+		return transPrice;
+	}
+
+	public String getCloseDate() {
+		return closeDate;
+	}
 
 }
