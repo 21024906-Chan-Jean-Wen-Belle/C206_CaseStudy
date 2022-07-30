e5ce86184687c72295a1ab4ff33bee42a5726163
21011471 <21011471@192.168.0.131>
2022-07-31 00:32:45 +0800

    bid class

--- C206_CaseStudy/src/Bid.java
+++ C206_CaseStudy/src/Bid.java
@@ -1,4 +1,38 @@
 
 public class Bid {
+	private String bidId;
+	private String itemName;
+	private String sellerEmail;
+	private String buyerEmail;
+	private double bidPrice;
+	
+	public Bid (String bidId, String itemName, 
+			String sellerEmail, String buyerEmail, double bidPrice) {
+		this.bidId = bidId;
+		this.itemName = itemName;
+		this.sellerEmail = sellerEmail;
+		this.buyerEmail = buyerEmail ;
+		this.bidPrice = bidPrice;
 
+	}
+
+	public String getBidId() {
+		return bidId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public String getSellerEmail() {
+		return sellerEmail;
+	}
+
+	public String getBuyerEmail() {
+		return buyerEmail;
+	}
+
+	public double getBidPrice() {
+		return bidPrice;
+	}
 }
