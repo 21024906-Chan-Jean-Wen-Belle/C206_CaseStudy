a2b3b0ee5088a9dfb5a690973bb721c2ce3cbff6
21011471 <21011471@192.168.0.131>
2022-07-31 00:20:57 +0800

    Item class

--- C206_CaseStudy/src/Item.java
+++ C206_CaseStudy/src/Item.java
@@ -1,4 +1,53 @@
 
 public class Item {
+	private String name;
+	private String description;
+	private double bidPrice;
+	private String startDate;
+	private String endDate;
+	private double increment;
+
+	
+	public Item (String name, String description, double bidPrice, 
+			String startDate, String endDate, double increment) {
+		this.name = name;
+		this.description = description;
+		this.bidPrice = bidPrice ;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.increment = increment ;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public double getBidPrice() {
+		return bidPrice;
+	}
+
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+
+	public double getIncrement() {
+		return increment;
+	}
 
 }
+
+
